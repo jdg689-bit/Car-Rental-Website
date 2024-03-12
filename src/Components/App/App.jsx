a86987a7002d510e39609adb5736bc800c84f123
Jacob de Graaf <jdgraaf689@gmail.com>
2024-03-12 11:48:30 +1000

    change: HashRouter

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from '../Navbar/Navbar.jsx'
 import Home from '../Home/Home'
