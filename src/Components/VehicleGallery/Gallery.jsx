c9d69a6e0f9c52e0d6bb3bd536591bc792a4e53f
Jacob de Graaf <jdgraaf689@gmail.com>
2024-03-12 15:00:02 +1000

    img changes

--- src/Components/VehicleGallery/Gallery.jsx
+++ src/Components/VehicleGallery/Gallery.jsx
@@ -1,4 +1,3 @@
-import {CAR_DATA} from './car_data.js'
 import './Gallery.css'
 import { useState } from 'react';
 // import carImg from '../../assets/vehicle-library/kluger.jpeg'
@@ -13,6 +12,64 @@ const DETAILS = {
   description: `5 Door Hatch, 5 Speed Automatic, Smartphone Connectivity, Reverse
   Camera/Sensors, Central Locking Power Windows`
 }
+
+const CAR_DATA = [{
+  id: 'COMPACT',
+  info: 'Suzuki Swift or similar',
+  img: {
+      path: './car-rental-deploy/src/assets/vehicle-library/kluger.jpeg',
+      alt: 'suzuki-swift'
+  }
+}, {
+  id: 'INTERMEDIATE',
+  info: 'Toyota Corolla Hatch or similar',
+  img: {
+      path: '../../assets/vehicle-library/kluger.jpeg',
+      alt: 'toyota-corolla-hatch'
+  }
+}, {
+  id: 'STANDARD',
+  info: 'Toyota Corolla Sedan or similar',
+  img: {
+      path: 'car-rental/main/src/assets/vehicle-library/kluger.jpeg',
+      alt: 'toyota-corolla-sedan'
+  }
+}, {
+  id: 'FULL-SIZE',
+  info: 'Toyota Camry or similar',
+  img: {
+      path: '/car-rental/assets/vehicle-library/2022-toyota-camry-accent-pacific-red_featured.avif',
+      alt: 'toyota-camry'
+  }
+}, {
+  id: 'COMPACT SUV',
+  info: 'Toyota C-HR or similar',
+  img: {
+      path: './src/assets/vehicle-library/2022-toyota-c-hr-nightshade-edition-suv-black_featured.avif',
+      alt: 'toyota-c-hr'
+  },
+}, {
+  id: 'FULL-SIZE SUV',
+  info: 'Toyota Kluger or similar',
+  img: {
+      path: 'src/assets/kluger.jpeg',
+      alt: 'toyota-kluger'
+  }
+}, {
+  id: 'FULL-SIZE VAN',
+  info: 'Kia Carnival or similar',
+  img: {
+      path: 'src/assets/vehicle-library/2022-kia-carnival-s-pacific-red_featured.avif',
+      alt: 'kia-carnival'
+  }
+}, {
+  id: 'SIGNATURE SERIES',
+  info: 'BMW X5 or similar',
+  img: {
+      path: 'src/assets/vehicle-library/2022-bmw-x5-xdrive40i-xline-design-4wd-suv-black_featured.avif',
+      alt: 'bmw-x5'
+  }
+}];
 
 
 
